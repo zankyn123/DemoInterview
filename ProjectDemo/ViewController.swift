7080c959b67d3a37bd95c323d946d553e0ed1307
Manh Hung <>
2024-10-24 22:36:15 +0700

    Add project with 14 ios minimun

---
+++ ProjectDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectDemo
+//
+//  Created by Manh Hung on 24/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
